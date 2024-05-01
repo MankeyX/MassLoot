c8bd3376bd588260d343a9d6645c97cb8260698c
mankeyx <charliestahlen@gmail.com>
2024-05-01 16:27:57 +0200

    Added IEquatable to LootItem

--- MassLoot/LootItem.cs
+++ MassLoot/LootItem.cs
@@ -5,7 +5,7 @@ namespace MassLoot;
 public class LootItem(
     string itemId,
     string weightExpression
-) : ILootItem
+) : ILootItem, IEquatable<LootItem>
 {
     public static readonly LootItem None = new("None", "0");
 
@@ -30,4 +30,22 @@ public class LootItem(
         IReadOnlyDictionary<string, double> variables
     ) =>
         Weight = _expression.Calculate(variables);
+
+    public bool Equals(LootItem? other)
+    {
+        if (ReferenceEquals(null, other))
+        {
+            return false;
+        }
+
+        return
+            ReferenceEquals(this, other) ||
+            ItemId == other.ItemId;
+    }
+
+    public override bool Equals(object? obj)
+        => obj is LootItem item && Equals(item);
+
+    public override int GetHashCode()
+        => ItemId.GetHashCode();
 }
